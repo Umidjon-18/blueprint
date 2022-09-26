1cd999e6fb2879cad0156d51cee34a7a832904fb
Umidjon Yoqubov <umidjonxomidjonovich@gmail.com>
2022-09-26 15:10:32 +0500

    blueprint completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,13 @@
 
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'busines_logic/cubits/change_color.dart';
-import 'presentation/pages/color_page/change_color_page.dart';
 
 void main() => runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_)=> ChangeColorCubit()),
-        ],
-        child: MaterialApp(
-        title: 'Flutter Demo',
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
           backgroundColor: Colors.white,
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: const ChangeColorPage(),
           ),
-      ),
     );
